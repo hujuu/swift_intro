dff4a91598e9497d85769115f42af28a6d6ba8ee
hujuu <out.of.the.base.sfc@gmail.com>
2016-05-15 02:53:40 +0900

    03-02を更新

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,10 +5,19 @@ import UIKit
 var str = "Hello, playground"
 var x = 3
 
+
 print(1 + 1)
 print(5-3)
 //地球の体積の計算
 print(4 / 4 * 3.14 * 6378 * 6378 * 6378) //地球の体積の計算
 
 print(str)
-print(x)
+print(x * x)
+
+//練習問題3-1
+var a = 2
+print(a * a)
+
+let tax = 1.08
+//tax = 1.05
+print(100 * tax)
