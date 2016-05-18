c60594c2ece41c5707177151871557a7b2939598
hujuu <out.of.the.base.sfc@gmail.com>
2016-05-19 02:10:45 +0900

    iAdの実装

---
+++ Ad/Ad/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ad
+//
+//  Created by Ken Nakai on 2016/05/19.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+import iAd
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.canDisplayBannerAds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
